de8229323c5f980984114fa25a08ec11a3fedf4b
Leo Blondel <xqua@free.fr>
2024-05-31 00:31:17 +0200

    adding translation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@
 import { ProgressBar } from "@/components/progressbar"
 import { StatCard } from "@/components/statcard"
 import { Progress } from "@/components/ui/progress"
-import { progresses, statsTable } from "@/data/data"
+import { progresses, progressesFR, statsTable } from "@/data/data"
+import { useTranslations } from "next-intl"
 import { useCallback, useEffect, useState } from "react"
 
 function interpolate(week: number, value1: number, value2: number): number {
@@ -16,11 +17,13 @@ export default function Home() {
 	const [currentWeek, setCurrentWeek] = useState(0)
 	const [currentWeight, setCurrentWeight] = useState(0)
 	const [currentHeight, setCurrentHeight] = useState(0)
+	const [progressesData, setProgressesData] = useState(progresses)
+	const t = useTranslations("Home")
 
-	// const currentWeight = statsTable[Math.round(weeksElasped)].weight
-	// const nextWeight = statsTable[Math.round(weeksElasped) + 1].weight
-	// const currentHeight = statsTable[Math.round(weeksElasped)].height
-	// const nextHeight = statsTable[Math.round(weeksElasped) + 1].height
+	useEffect(() => {
+		const userLocale = navigator.language
+		setProgressesData(userLocale.includes("fr") ? progressesFR : progresses)
+	}, [])
 
 	const getCellCount = useCallback(() => {
 		let cellCount = currentWeight / 0.00000001
@@ -58,25 +61,25 @@ export default function Home() {
 		<main className="flex min-h-screen flex-col items-center justify-between p-4 lg:p-12 gap-4 bg-slate-200">
 			<div className="border-2 border-gray-300 shadow-md bg-white p-4 rounded-2xl">
 				<div className="flex flex-col items-center gap-4 w-full pb-12">
-					<h1 className="text-4xl font-bold text-center">SaraLeo baby&apos;s progress bar</h1>
+					<h1 className="text-4xl font-bold text-center">{t("title")}</h1>
 					<div className="relative items-center w-full">
 						<Progress value={(currentWeek / 40) * 100} className="h-8" />
 						<span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xl font-bold text-center text-white">
 							{((currentWeek / 40) * 100).toFixed(2)}%
 						</span>
 					</div>
-					<h2 className="text-2xl font-bold text-center">Baby compiling ... Please wait</h2>
+					<h2 className="text-2xl font-bold text-center">{t("subtitle")}</h2>
 				</div>
 				<div className="flex flex-col items-center gap-4 w-full">
 					<div className="grid grid-cols-1 md:grid-cols-2 lg:flex lg:flex-row lg:justify-center items-center gap-2 w-full flex-wrap">
-						<StatCard metric={Math.round(daysElapsed)} label="Days since DNA mixing" />
-						<StatCard metric={Math.round(currentWeek)} label="Current Week" />
-						<StatCard metric={currentWeight.toFixed(2)} label="Estimated Weight (in g)" />
-						<StatCard metric={currentHeight.toFixed(2)} label="Estimated Size (in cm)" />
-						<StatCard metric={getCellCount()} label="Estimated number of cells" />
+						<StatCard metric={Math.round(daysElapsed)} label={t("mixing")} />
+						<StatCard metric={Math.round(currentWeek)} label={t("curweek")} />
+						<StatCard metric={currentWeight.toFixed(2)} label={t("estweight")} />
+						<StatCard metric={currentHeight.toFixed(2)} label={t("estsize")} />
+						<StatCard metric={getCellCount()} label={t("estcells")} />
 					</div>
 					<div className="flex flex-col gap-4 w-full">
-						{progresses.map((progress, index) => (
+						{progressesData.map((progress, index) => (
 							<ProgressBar key={index} currentWeek={currentWeek} progress={progress} />
 						))}
 					</div>
--- src/data/data.ts
+++ src/data/data.ts
@@ -39,179 +39,355 @@ export type ProgressType = {
 	startWeek: number
 	endWeek: number
 }
-
 export const progresses = [
 	{
-		label: "Survival limit reached!",
-		inProgressLabel: "Absolute survival limit in progress ...",
+		label: "Survival Limit Reached!",
+		inProgressLabel: "Reaching Absolute Survival Limit...",
 		startWeek: 1,
 		endWeek: 23
 	},
 	{
 		label: "Implantation Complete!",
-		inProgressLabel: "Embryo looking for a nest ...",
+		inProgressLabel: "Embryo Seeking Nest...",
 		startWeek: 1,
 		endWeek: 3
 	},
 	{
-		label: "Heart Complete!",
-		inProgressLabel: "Heart Compiling!",
+		label: "Heart Compiled!",
+		inProgressLabel: "Heart Under Construction...",
 		startWeek: 1,
 		endWeek: 5
 	},
 	{
-		label: "Neural-tube closed!",
-		inProgressLabel: "Neural-tube Compiling!",
+		label: "Neural-Tube Closed!",
+		inProgressLabel: "Neural-Tube Formation In Progress...",
 		startWeek: 1,
 		endWeek: 6
 	},
 	{
-		label: "Movement aquired!",
-		inProgressLabel: "Movement compiling ...",
+		label: "Movement Acquired!",
+		inProgressLabel: "Uploading Movement Protocols...",
 		startWeek: 1,
 		endWeek: 11
 	},
 	{
-		label: "Fingerprints set!",
-		inProgressLabel: "Fingerprints compiling ...",
+		label: "Fingerprints Set!",
+		inProgressLabel: "Fingerprint Patterns Developing...",
 		startWeek: 1,
 		endWeek: 7
 	},
 	{
 		label: "Organogenesis Complete!",
-		inProgressLabel: "Organogenesis compiling ...",
+		inProgressLabel: "Organogenesis Underway...",
 		startWeek: 1,
 		endWeek: 13
 	},
 	{
-		label: "Teeth set!",
-		inProgressLabel: "Teeth compiling ...",
+		label: "Teeth Set!",
+		inProgressLabel: "Dental Records Being Established...",
 		startWeek: 9,
 		endWeek: 20
 	},
 	{
-		label: "Genitalia set!",
-		inProgressLabel: "Genitalia compiling ...",
+		label: "Genitalia Set!",
+		inProgressLabel: "Genitalia Forming...",
 		startWeek: 10,
 		endWeek: 14
 	},
 	{
-		label: "Vocal cords set!",
-		inProgressLabel: "Vocal cords compiling ...",
+		label: "Vocal Cords Set!",
+		inProgressLabel: "Vocal Cords Developing...",
 		startWeek: 13,
 		endWeek: 14
 	},
 	{
 		label: "Toenails Complete!",
-		inProgressLabel: "Toenails compiling ...",
+		inProgressLabel: "Toenails Growing...",
 		startWeek: 10,
 		endWeek: 14
 	},
 	{
-		label: "Fingers set!",
-		inProgressLabel: "Finger compiling ...",
+		label: "Fingers Set!",
+		inProgressLabel: "Finger Formation In Progress...",
 		startWeek: 14,
 		endWeek: 20
 	},
 	{
-		label: "It can ear!",
-		inProgressLabel: "Audition compiling ...",
+		label: "Auditory System Activated!",
+		inProgressLabel: "Hearing Abilities Developing...",
 		startWeek: 15,
 		endWeek: 16
 	},
 	{
-		label: "It has ears!",
-		inProgressLabel: "Ears compiling ...",
+		label: "Ears Formed!",
+		inProgressLabel: "Ear Construction In Progress...",
 		startWeek: 16,
 		endWeek: 19
 	},
 	{
-		label: "It has a nose!",
-		inProgressLabel: "Nose compiling ...",
+		label: "Nose Formed!",
+		inProgressLabel: "Nose Taking Shape...",
 		startWeek: 16,
 		endWeek: 19
 	},
 	{
-		label: "It has lips!",
-		inProgressLabel: "Lips compiling ...",
+		label: "Lips Formed!",
+		inProgressLabel: "Lips Developing...",
 		startWeek: 17,
 		endWeek: 19
 	},
 	{
-		label: "It has hairs (Weird)!",
-		inProgressLabel: "Hairs compiling ...",
+		label: "Hair Growth Initiated!",
+		inProgressLabel: "Hair Sprouting...",
 		startWeek: 14,
 		endWeek: 20
 	},
 	{
-		label: "It makes its own antibodies!",
-		inProgressLabel: "Immune system compiling ...",
+		label: "Immune System Activated!",
+		inProgressLabel: "Immune Defense System Online...",
 		startWeek: 18,
 		endWeek: 20
 	},
 	{
-		label: "It Kicks!",
-		inProgressLabel: "Kicking system compiling ...",
+		label: "Kick Detected!",
+		inProgressLabel: "Kicking Abilities Booting Up...",
 		startWeek: 18,
 		endWeek: 20
 	},
 	{
-		label: "It dreams of electric sheeps (REM detectable)!",
-		inProgressLabel: "Dream system compiling ...",
+		label: "Dreams Detected (REM Active)!",
+		inProgressLabel: "Dream System Activating...",
 		startWeek: 18,
 		endWeek: 20
 	},
 	{
-		label: "Lung Complete!",
-		inProgressLabel: "Lung compiling ...",
+		label: "Lung Development Complete!",
+		inProgressLabel: "Lung Formation Underway...",
 		startWeek: 15,
 		endWeek: 25
 	},
 	{
-		label: "It has eyelashes!",
-		inProgressLabel: "Eyelashes compiling ...",
+		label: "Eyelashes Formed!",
+		inProgressLabel: "Eyelash Growth In Progress...",
 		startWeek: 20,
 		endWeek: 28
 	},
 	{
-		label: "It has bone marrow!",
-		inProgressLabel: "Bone marrow compiling ...",
+		label: "Bone Marrow Formed!",
+		inProgressLabel: "Bone Marrow Production...",
 		startWeek: 9,
 		endWeek: 21
 	},
 	{
-		label: "It has big muscle!",
-		inProgressLabel: "Muscles compiling ...",
+		label: "Muscles Developed!",
+		inProgressLabel: "Muscle Mass Building...",
 		startWeek: 9,
 		endWeek: 33
 	},
 	{
-		label: "It has some fat!",
-		inProgressLabel: "Bodyfat compiling ...",
+		label: "Body Fat Accumulated!",
+		inProgressLabel: "Body Fat Deposits Growing...",
 		startWeek: 17,
 		endWeek: 33
 	},
 	{
-		label: "Heating system aquired!",
-		inProgressLabel: "Heating system compiling ...",
+		label: "Heating System Online!",
+		inProgressLabel: "Thermoregulation System Activating...",
 		startWeek: 28,
 		endWeek: 30
 	},
 	{
-		label: "It has some shaky bones!",
-		inProgressLabel: "Bones compiling ...",
+		label: "Bones Strengthened!",
+		inProgressLabel: "Bone Hardening...",
 		startWeek: 1,
 		endWeek: 39
 	},
 	{
-		label: "Ready for birth!",
-		inProgressLabel: "Getting ready for launch ...",
+		label: "Ready for Launch!",
+		inProgressLabel: "Preparing for Birth...",
 		startWeek: 38,
 		endWeek: 40
 	},
 	{
-		label: "Brain kinda formed!",
-		inProgressLabel: "Brain compiling ...",
+		label: "Brain Nearly Complete!",
+		inProgressLabel: "Brain Development In Progress...",
+		startWeek: 6,
+		endWeek: 39
+	}
+] as ProgressType[]
+
+export const progressesFR = [
+	{
+		label: "Limite de survie atteinte !",
+		inProgressLabel: "Atteinte de la limite de survie absolue en cours...",
+		startWeek: 1,
+		endWeek: 23
+	},
+	{
+		label: "Implantation terminée !",
+		inProgressLabel: "L'embryon cherche un nid...",
+		startWeek: 1,
+		endWeek: 3
+	},
+	{
+		label: "Cœur compilé !",
+		inProgressLabel: "Construction du cœur en cours...",
+		startWeek: 1,
+		endWeek: 5
+	},
+	{
+		label: "Tube neural fermé !",
+		inProgressLabel: "Formation du tube neural en cours...",
+		startWeek: 1,
+		endWeek: 6
+	},
+	{
+		label: "Mouvement acquis !",
+		inProgressLabel: "Téléchargement des protocoles de mouvement...",
+		startWeek: 1,
+		endWeek: 11
+	},
+	{
+		label: "Empreintes digitales établies !",
+		inProgressLabel: "Développement des motifs d'empreintes digitales...",
+		startWeek: 1,
+		endWeek: 7
+	},
+	{
+		label: "Organogenèse terminée !",
+		inProgressLabel: "Organogenèse en cours...",
+		startWeek: 1,
+		endWeek: 13
+	},
+	{
+		label: "Dents formées !",
+		inProgressLabel: "Établissement des dossiers dentaires...",
+		startWeek: 9,
+		endWeek: 20
+	},
+	{
+		label: "Appareil génital formé !",
+		inProgressLabel: "Formation de l'appareil génital en cours...",
+		startWeek: 10,
+		endWeek: 14
+	},
+	{
+		label: "Cordes vocales formées !",
+		inProgressLabel: "Développement des cordes vocales...",
+		startWeek: 13,
+		endWeek: 14
+	},
+	{
+		label: "Ongles des orteils terminés !",
+		inProgressLabel: "Croissance des ongles des orteils en cours...",
+		startWeek: 10,
+		endWeek: 14
+	},
+	{
+		label: "Doigts formés !",
+		inProgressLabel: "Formation des doigts en cours...",
+		startWeek: 14,
+		endWeek: 20
+	},
+	{
+		label: "Système auditif activé !",
+		inProgressLabel: "Développement des capacités auditives...",
+		startWeek: 15,
+		endWeek: 16
+	},
+	{
+		label: "Oreilles formées !",
+		inProgressLabel: "Construction des oreilles en cours...",
+		startWeek: 16,
+		endWeek: 19
+	},
+	{
+		label: "Nez formé !",
+		inProgressLabel: "Formation du nez en cours...",
+		startWeek: 16,
+		endWeek: 19
+	},
+	{
+		label: "Lèvres formées !",
+		inProgressLabel: "Développement des lèvres en cours...",
+		startWeek: 17,
+		endWeek: 19
+	},
+	{
+		label: "Croissance des cheveux initiée !",
+		inProgressLabel: "Poussée des cheveux en cours...",
+		startWeek: 14,
+		endWeek: 20
+	},
+	{
+		label: "Système immunitaire activé !",
+		inProgressLabel: "Système de défense immunitaire en ligne...",
+		startWeek: 18,
+		endWeek: 20
+	},
+	{
+		label: "Coup de pied détecté !",
+		inProgressLabel: "Initialisation des capacités de coup de pied...",
+		startWeek: 18,
+		endWeek: 20
+	},
+	{
+		label: "Rêves détectés (REM actifs) !",
+		inProgressLabel: "Activation du système de rêves...",
+		startWeek: 18,
+		endWeek: 20
+	},
+	{
+		label: "Développement des poumons terminé !",
+		inProgressLabel: "Formation des poumons en cours...",
+		startWeek: 15,
+		endWeek: 25
+	},
+	{
+		label: "Cils formés !",
+		inProgressLabel: "Croissance des cils en cours...",
+		startWeek: 20,
+		endWeek: 28
+	},
+	{
+		label: "Moelle osseuse formée !",
+		inProgressLabel: "Production de moelle osseuse en cours...",
+		startWeek: 9,
+		endWeek: 21
+	},
+	{
+		label: "Muscles développés !",
+		inProgressLabel: "Construction de la masse musculaire...",
+		startWeek: 9,
+		endWeek: 33
+	},
+	{
+		label: "Accumulation de graisse corporelle !",
+		inProgressLabel: "Croissance des dépôts de graisse corporelle...",
+		startWeek: 17,
+		endWeek: 33
+	},
+	{
+		label: "Système de chauffage en ligne !",
+		inProgressLabel: "Activation du système de thermorégulation...",
+		startWeek: 28,
+		endWeek: 30
+	},
+	{
+		label: "Renforcement des os !",
+		inProgressLabel: "Durcissement des os en cours...",
+		startWeek: 1,
+		endWeek: 39
+	},
+	{
+		label: "Prêt pour le lancement !",
+		inProgressLabel: "Préparation pour la naissance...",
+		startWeek: 38,
+		endWeek: 40
+	},
+	{
+		label: "Cerveau presque complet !",
+		inProgressLabel: "Développement du cerveau en cours...",
 		startWeek: 6,
 		endWeek: 39
 	}
